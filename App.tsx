f1727224f92283d678758556aa57a3cdbf92fd48
Cees Wang <wangcees@gmail.com>
2020-08-19 18:52:51 -0400

    added bottom navigator bar

--- App.tsx
+++ App.tsx
@@ -1,21 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import CreateScreen from './screens/CreateScreen';
+import DiscoverScreen from './screens/DiscoverScreen';
+import HomeScreen from './screens/HomeScreen';
+import InboxScreen from './screens/InboxScreen';
+import ProfileScreen from './screens/ProfileScreen'; 
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen}/>
+        <Tab.Screen name="Discover" component={DiscoverScreen}/>
+        <Tab.Screen name="Inbox" component={InboxScreen}/>
+        <Tab.Screen name="Create" component={CreateScreen}/>
+        <Tab.Screen name="Profile" component={ProfileScreen}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/InboxScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+const InboxScreen = () => {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>INOBX!</Text>
+      </View>
+    );
+}
+
+export default InboxScreen;
